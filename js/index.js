a8cb668d8ee3fdfb2b825d6266f20015addb841e
Ashish12meena <124426777+Ashish12meena@users.noreply.github.com>
2023-08-07 08:53:51 +0530

    Add files via upload

---
+++ js/index.js
@@ -0,0 +1,21 @@
+console.log("My name is Ashish");
+let para = document.getElementById('learn-more');
+let btn = document.getElementsByClassName('btn');
+console.log(btn);
+console.log(para);
+function hide() {
+    if (para.style.display != 'none') {
+        para.style.display = 'none';
+    } else {
+        para.style.display = 'block';
+    }
+}
+let sign_in = document.getElementById('Sign-in');
+var data="Sign in";
+var dataleave= sign_in.innerHTML;
+function sign() {
+    sign_in.innerHTML=data;
+}
+function signleave() {
+    sign_in.innerHTML=dataleave;
+}
