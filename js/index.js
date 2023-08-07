6ec95beddd12a5d6b8e5a80eaf24310790768452
Ashish12meena <124426777+Ashish12meena@users.noreply.github.com>
2023-08-07 09:24:48 +0530

    Update index.js

--- js/index.js
+++ js/index.js
@@ -6,10 +6,8 @@ console.log(para);
 function hide() {
     if (para.style.display = 'none') {
         para.style.display = 'block';
-        console.log("display is block");
     } else {
         para.style.display = 'none';
-        console.log("display is none");
     }
 }
 let sign_in = document.getElementById('Sign-in');
@@ -17,6 +15,7 @@ var data="Sign in";
 var dataleave= sign_in.innerHTML;
 function sign() {
     sign_in.innerHTML=data;
+    console.log("sign in");
 }
 function signleave() {
     sign_in.innerHTML=dataleave;
